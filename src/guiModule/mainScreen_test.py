f92e7edf4f49c6e11a31340df13ecda9f5679795
Nicolas Diaz <ndiaz2@trinity.edu>
2026-02-15 22:32:49 -0600

    removed pytest from pre-commit because it was causing issues. Will likely have to add it to CI testing

--- src/guiModule/mainScreen_test.py
+++ src/guiModule/mainScreen_test.py
@@ -4,18 +4,14 @@
 
 # pylint: disable=invalid-name
 import mainScreen
-from pytestqt import *
-
 
 
 # Test to see if the user can type in editableContentText
 # pylint: disable=invalid-name
-
 def test_EditableContentText(qtbot)->None: # type: ignore
     # An instance of MainWindow used for testing
     testMainScreen: mainScreen.MainWindow = mainScreen.MainWindow()
     qtbot.keyClicks(testMainScreen.editableContentText, "It worked")
-
     assert (
         testMainScreen.editableContentText.toPlainText() == "It worked"
     ), "Text typed into editableContentText does not match expected text"
